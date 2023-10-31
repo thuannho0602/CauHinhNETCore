114c62ea18aec02720a569255ee66112cdcc6578
thuannguyen <thuannho0602@gmail.com>
2023-10-31 23:20:43 +0700

    sdsdsdsds

---
+++ CauHinhNET.Model/Clinets/ClientCreateResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CauHinhNET.Model.Clinets
+{
+    public class ClientCreateResponse
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string AddRess { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
