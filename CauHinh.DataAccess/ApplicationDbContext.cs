097b599e021de91143800cc18dc31ca383994daa
thuannguyen <thuannho0602@gmail.com>
2023-10-31 23:10:57 +0700

    newdatabase

---
+++ CauHinh.DataAccess/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using CauHinhNET.Entity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CauHinhNET.DataAccess
+{
+    public class ApplicationDbContext:IdentityDbContext<IdentityUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public virtual DbSet<Client> Clients { get; set; } 
+    }
+}
---
+++ CauHinhNET.Entity/Client.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CauHinhNET.Entity
+{
+    [Table("Client")]
+    public class Client
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string AddRess { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
